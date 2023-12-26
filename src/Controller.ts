1990832f7b522c55340965a5da704b613add75b1
Andrey Dokuchayev <andreydoku@gmail.com>
2023-12-26 00:11:56 -0600

    move everything to controller

--- src/Controller.ts
+++ src/Controller.ts
@@ -4,15 +4,150 @@ import { Service } from "./Service";
 
 const service:Service = new Service();
 
-// /todos
-export const getAllTodos: APIGatewayProxyHandler = async (_event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+//   /todo/{todoId}
+export const getTodo: APIGatewayProxyHandler = async(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	
+	const id:string|undefined = event?.pathParameters?.todoId;
+	if( id == undefined ){
+		return {
+			statusCode: 400,
+			body: JSON.stringify({
+				message: "ID path parameter required",
+			})
+		};
+	}
+	
 	try {
 		
-		const Todos:Todo[] = await service.getAllTodos();
+		const todo:Todo = await service.getTodo(id);
 		
 		const response = {
 			statusCode: 200,
-			body: JSON.stringify( Todos )
+			body: JSON.stringify( todo )
+		};
+		return response;
+	}
+	catch(e: any) {
+
+		console.error(e);
+
+		return {
+			statusCode: 500,
+			body: JSON.stringify({
+				message: "Failed to get todo.",
+				errorMsg: e.message,
+				errorStack: e.stack,
+			})
+		};
+	}
+	
+}
+
+//   /todo
+export const createTodo: APIGatewayProxyHandler = async(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	
+	if(!event.body ){
+		return {
+			statusCode: 400,
+			body: JSON.stringify({
+				message: "Request body must contain Todo data",
+			})
+		};
+	}
+	
+	const requestBody = JSON.parse(event.body);
+	const todo:Todo = {
+		todoId: null, 
+		title: requestBody.title, 
+		isDone: requestBody.isDone
+	};
+	
+	if( !todo.title ){
+		return {
+			statusCode: 400,
+			body: JSON.stringify({
+				message: "Request body is missing param title"
+			})
+		};
+	}
+	
+	
+	try {
+		
+		const createdTodo:Todo = await service.createTodo(todo);
+		
+		return {
+			statusCode: 200,
+			body: JSON.stringify( createdTodo )
+		};
+		
+	}
+	catch(e: any) {
+
+		console.error(e);
+
+		return {
+			statusCode: 500,
+			body: JSON.stringify({
+				message: "Failed to create todo.",
+				errorMsg: e.message,
+				errorStack: e.stack,
+			})
+		};
+	}
+	
+}
+
+//   /todo/{todoId}
+export const deleteTodo: APIGatewayProxyHandler = async(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	
+	const id:string|undefined = event?.pathParameters?.todoId;
+	if( id == undefined ){
+		return {
+			statusCode: 400,
+			body: JSON.stringify({
+				message: "ID path parameter required",
+			})
+		};
+	}
+	
+	
+	try {
+		
+		const deletedTodo:Todo = await service.deleteTodo(id);
+		
+		return {
+			statusCode: 200,
+			body: JSON.stringify( deletedTodo )
+		};
+		
+	}
+	catch(e: any) {
+
+		console.error(e);
+
+		return {
+			statusCode: 500,
+			body: JSON.stringify({
+				message: "Failed to create todo.",
+				errorMsg: e.message,
+				errorStack: e.stack,
+			})
+		};
+	}
+	
+}
+
+
+//   /todos
+export const getAllTodos: APIGatewayProxyHandler = async (_event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	try {
+		
+		const todos:Todo[] = await service.getAllTodos();
+		
+		const response = {
+			statusCode: 200,
+			body: JSON.stringify( todos )
 		};
 		return response;
 	}
--- src/Service.ts
+++ src/Service.ts
@@ -1,23 +1,69 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-const db = new DynamoDBClient({});
-import { GetItemCommand, PutItemCommand, DeleteItemCommand, ScanCommand, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, GetItemCommand, PutItemCommand, DeleteItemCommand, ScanCommand, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
 import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 import { uuid } from 'uuidv4';
-
 import { Todo } from "./Todo";
 
+const db = new DynamoDBClient({});
+
 export class Service{
+	
+	async getTodo(id:string) : Promise<Todo> {
+		
+		const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Key: marshall({ todoId: id }),
+        };
+        let { Item } = await db.send(new GetItemCommand(params));
+		
+		if( !Item ){
+			throw new Error("failed to get stuff from DB");
+		}
+		
+		const unmarshalledItem = unmarshall(Item);
+		
+		const todo:Todo = {
+			todoId: unmarshalledItem.todoId, 
+			title: unmarshalledItem.title, 
+			isDone: unmarshalledItem.isDone
+		};
+		return todo;
+		
+	}
+	async createTodo( todo:Todo ) : Promise<Todo> {
+		
+		todo.todoId = uuid();
+		
+		const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Item: marshall(todo || {}),
+        };
+		
+		await db.send(new PutItemCommand(params));
+		
+		return todo;
+		
+	}
+	async deleteTodo( id:string ): Promise<Todo>{
+		
+		const todo:Todo = await this.getTodo(id);
+		
+		const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Key: marshall({ todoId: id }),
+        };
+        await db.send(new DeleteItemCommand(params));
+		
+		return todo;
+		
+	}
+	
+	
 	async getAllTodos() : Promise<Todo[]> {
 		
-		// const dummyTodos: Todo[] = [
-		// 	{ todoId: "1", title: "sweep floors"    , isDone: false },
-		// 	{ todoId: "2", title: "grocery shopping", isDone: false },
-		// 	{ todoId: "3", title: "clean bathroom"  , isDone: false },
-		// ];
-		
-		// return Promise.resolve( dummyTodos );
-		
-		const { Items } = await db.send(new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME }));
+		const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME
+        };
+		const { Items } = await db.send(new ScanCommand(params));
 		
 		
 		if( !Items ){
@@ -26,11 +72,13 @@ export class Service{
 		
 		
 		const todos:Todo[] = Items
-			.map((item) => unmarshall(item))
+			.map( item => unmarshall(item) )
 			.map( item => { 
 				return {todoId: item.todoId, title:item.title, isDone: item.isDone} 
 			});
 		
 		return todos;
 	}
+	
+	
 }
